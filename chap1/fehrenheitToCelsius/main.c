291652535c220c104026578ded99468f1708eb1b
Milton <miltonio94@gmail.com>
2024-03-18 10:18:31 +0000

    new notes and example programs

--- chap1/fehrenheitToCelsius/main.c
+++ chap1/fehrenheitToCelsius/main.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
 
+#define LOWER 0
+#define UPPER 300
+#define STEP 20
+
 // Print fahrenheit to celsius table
 int main() {
-  int fahr = 0, celsius = 0;
+  int fahr;
 
-  int lower = 0;
-  int upper = 300;
-  int step = 20;
-
-  while (fahr <= upper) {
-    celsius = 5 * (fahr - 32) / 9;
-    printf("%3d %6d\n", fahr, celsius);
-    fahr += step;
+  for (fahr = 0; fahr <= UPPER; fahr += STEP) {
+    printf("%3d %6.1f\n", fahr, (5. / 9.) * (fahr - 32));
   }
 }
